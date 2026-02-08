b189bdb660f2ec571e12332769b2daedb2de28e6
Nirvana <Nirvana@gmail.com>
2026-02-08 21:08:59 +0100

    Reformat

--- service.py
+++ service.py
@@ -743,14 +743,15 @@ class UltimateService:
                     # Map all video (will be just one due to highest_quality_only), all audio, and optional subtitles
                     ffmpeg_cmd = (
                         f'pipe://ffmpeg -loglevel fatal '
-                        f'-re '
                         f'-fflags +genpts+discardcorrupt '
                         f'-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 2 '
                         f'-i "{stream_url}" '
                         f'-map 0:v -map 0:a? '
                         f'-c copy '
+                        f'-avoid_negative_ts make_zero '
                         f'-f mpegts '
                         f'-flush_packets 1 '
+                        f'-use_wallclock_as_timestamps 1 '
                         f'-mpegts_flags resend_headers '
                         f'-metadata service_name="{channel_name}" '
                         f'pipe:1'
