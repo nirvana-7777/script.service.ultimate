095598beee1404b00f2ee46b438748910adcf530
Nirvana <Nirvana@gmail.com>
2026-03-08 21:04:16 +0100

    Add discovery

--- lib/streaming_providers/providers/rtlplus/provider.py
+++ lib/streaming_providers/providers/rtlplus/provider.py
@@ -420,7 +420,7 @@ class RTLPlusProvider(StreamingProvider):
                             drm_configs.append(
                                 create_drm_config(
                                     DRMSystem.WIDEVINE,
-                                    1,
+                                    2,
                                     license_url,
                                     self.rtl_config.get_drm_headers(access_token),
                                 )
@@ -429,7 +429,7 @@ class RTLPlusProvider(StreamingProvider):
                             drm_configs.append(
                                 create_drm_config(
                                     DRMSystem.PLAYREADY,
-                                    2,
+                                    1,
                                     license_url,
                                     self.rtl_config.get_playready_drm_headers(access_token),
                                 )
